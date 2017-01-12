b1e3d775b08fb3cd6a56bbf937367e26c3cb93a3
Hugo Digiovanni <d14000402@tempo21-2.luminy.univ-amu.fr>
2017-01-12 11:41:55 +0100

    modifs du 12/01/2016

--- main.c
+++ main.c
@@ -23,9 +23,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "constants.h"
+
+#define NOMBRE_DE_CHOIX 3
+
 
 int main()
 {
+    int choix;
+    char fget;
+
+    do
+    {
+        system(CLEAR);
+        printf("\n");
+        printf("\t\t--------------------\n");
+        printf("\t\t|Hoffman  Compressor|\n");  
+        printf("\t\t--------------------\n");
+        printf("\n");
+        printf("\n");
+        printf("\tQue voulez vous faire ?\n");
+        printf("\t\n");
+        printf("\t1 - Compresser un fichier\n");
+        printf("\t2 - Decompresser un fichier\n");
+        printf("\t3 - Compresser un message (dans un fichier)\n");
+        printf("\t0 - Quitter\n");
+        printf("\t\n");
+        printf("\tChoix :");
+        scanf("%d", &choix);
+        if (!(choix >= 0 && choix < NOMBRE_DE_CHOIX)){
+            system(CLEAR);
+            printf("\n\n\t choix inconnu ...\n\n");
+            printf("appuyez sur entree pour continuer...");
+            system(SYSREAD);
+        } 
+    }while (choix !=0);
+    system(CLEAR);
+    printf("\t\n");
+    printf("\t\taurevoir !\n");
+    printf("\n");
+    printf("appuyez sur entree pour continuer...");
+    system(SYSREAD);
+    system(CLEAR);
+
+    /*
     // On ouvre le fichier en mode lecture
     FILE *file = fopen("../test.txt", "r");
     if (file != NULL)
@@ -46,5 +87,6 @@ int main()
         perror("test.txt");
     }
     return EXIT_SUCCESS;
+    */
 }
 
