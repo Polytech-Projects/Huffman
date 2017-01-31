b5db94b7be32964b6f4de91ac870ede1051e9638
BarbDev <tanguy.poret@etu.univ-amu.fr>
2017-01-31 21:20:41 +0100

    Corrections - erreur de type/signature

--- arbre.h
+++ arbre.h
@@ -81,7 +81,7 @@ char elem_valeur(tpn elem);
 int elem_ordre(tpn elem);
 int elem_poids(tpn elem);
 void permuter(tpn elem1, tpn elem2, t_arbre *arbre);
-void liberer_arbre(t_arbre *arbre);
+void liberer_arbre(tpn arbre);
 int nb_feuilles(tpn a);
 int profondeur(tpn arbre);
 
